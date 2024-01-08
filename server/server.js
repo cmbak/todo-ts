06f284b2d8dc79a2c616652aec072e516efcb655
cmbak <chikmii@gmail.com>
2024-01-08 22:25:57 +0000

    Linked db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT;
+const pool = require('./queries');
+
+app.get('/', async (res, req) => {
+    try {
+        console.log('get!');
+    } catch (error) {
+        console.error(error.message);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
