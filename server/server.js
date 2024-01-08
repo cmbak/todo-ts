54510f56326f53f5937f28b3186cda14bc1e1660
cmbak <chikmii@gmail.com>
2024-01-08 23:18:47 +0000

    Added route to get todo by user id

--- server/server.js
+++ server/server.js
@@ -4,10 +4,25 @@ const app = express();
 const PORT = process.env.SERVER_PORT;
 const pool = require('./queries');
 
+// GET routes
+
 // do i need async await?
-app.get('/', async (req, res) => {
+app.get('/todos', async (req, res) => {
     try {
-        const response = await pool.query('SELECT * FROM todo, todo_user;');
+        const response = await pool.query('SELECT * FROM todo;');
+        res.status(200).json(response.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+app.get('/todos/:userId', async (req, res) => {
+    try {
+        const { user_id } = req.params;
+        const response = await pool.query(
+            'SELECT * FROM todo WHERE user_id = $1;',
+            [user_id]
+        );
         res.status(200).json(response.rows);
     } catch (error) {
         console.error(error.message);
