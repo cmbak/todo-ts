27d9d32cb2a3953251bed409cae0f95f3e938e04
cmbak <chikmii@gmail.com>
2024-01-09 00:18:43 +0000

    Added styling to edit and delete buttons

--- client/todo-ts/components/DeleteTodoBtn.tsx
+++ client/todo-ts/components/DeleteTodoBtn.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function DeleteTodoBtn() {
-    return <button>DeleteTodoBtn</button>;
+    return <button className="todo-btn delete-btn">DeleteTodoBtn</button>;
 }
--- client/todo-ts/components/EditTodoBtn.tsx
+++ client/todo-ts/components/EditTodoBtn.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function EditTodoBtn() {
-    return <button>EditTodoBtn</button>;
+    return <button className="todo-btn edit-btn">EditTodoBtn</button>;
 }
