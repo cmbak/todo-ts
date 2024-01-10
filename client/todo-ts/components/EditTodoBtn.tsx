277d3b9cc02b34b02f5d138a1b0fcbb1146a3dae
cmbak <chikmii@gmail.com>
2024-01-10 17:02:57 +0000

    Added toggle for showing edit modal

--- client/todo-ts/components/EditTodoBtn.tsx
+++ client/todo-ts/components/EditTodoBtn.tsx
@@ -5,11 +5,12 @@ export default function EditTodoBtn({ todo }: TodoBtnProps) {
     const [name, setName] = useState(todo.name);
     const [description, setDescription] = useState(todo.description || ''); // Need ''?
     // todo date?
+    const [showModal, setShowModal] = useState(false);
 
-    function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+    async function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
         event.preventDefault();
 
-        fetch(`http://localhost:3000/todos/${todo.todo_id}`, {
+        await fetch(`http://localhost:3000/todos/${todo.todo_id}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -19,29 +20,42 @@ export default function EditTodoBtn({ todo }: TodoBtnProps) {
         });
     }
 
+    function handleClick() {
+        console.log(todo.todo_id);
+        setShowModal((prevShowModal) => !prevShowModal);
+    }
+
     return (
         <>
-            <button className="todo-btn edit-btn">Edit Todo</button>
-            <div className="edit-btn-modal">
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        name="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        name="description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                    <button type="submit" className="edit-btn">
-                        Edit Todo
-                    </button>
-                    {/* <input type="text" name="due-date" id="" /> */}
-                </form>
-            </div>
+            <button className="todo-btn edit-btn" onClick={handleClick}>
+                Edit Todo
+            </button>
+            {/* TODO make modal separate component? */}
+            {showModal && (
+                <div
+                    className="edit-btn-modal"
+                    id={`edit-btn-modal-${todo.todo_id}`}
+                >
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            name="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <button type="submit" className="edit-btn">
+                            Edit Todo
+                        </button>
+                        {/* <input type="text" name="due-date" id="" /> */}
+                    </form>
+                </div>
+            )}
         </>
     );
 }
