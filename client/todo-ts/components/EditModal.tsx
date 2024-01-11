9fa1081ad447bd80016b5ccac4a986b622c94344
cmbak <chikmii@gmail.com>
2024-01-11 17:24:20 +0000

    Added overlay

--- client/todo-ts/components/EditModal.tsx
+++ client/todo-ts/components/EditModal.tsx
@@ -14,19 +14,22 @@ interface EditModalProps {
 
 export default function EditModal({ todo }: EditModalProps) {
     return (
-        <div className="modal">
-            <div className="modal-content">
-                <form>
-                    <input type="text" name="name" value={todo.name} />
-                    <input
-                        type="text"
-                        name="description"
-                        value={todo.description}
-                    />
-                </form>
+        <>
+            <div className="modal">
+                <div className="modal-content">
+                    <form>
+                        <input type="text" name="name" value={todo.name} />
+                        <input
+                            type="text"
+                            name="description"
+                            value={todo.description}
+                        />
+                    </form>
+                </div>
+                <button className="edit-todo-btn">Edit Todo</button>
+                <button className="cancel-btn">Cancel</button>
             </div>
-            <button className="edit-todo-btn">Edit Todo</button>
-            <button className="cancel-btn">Cancel</button>
-        </div>
+            <div className="overlay"></div>
+        </>
     );
 }
