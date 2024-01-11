909f17cf3a495cc32d1293955040e9b0a1637247
cmbak <chikmii@gmail.com>
2024-01-11 17:15:20 +0000

    Centred modal

--- client/todo-ts/components/EditModal.tsx
+++ client/todo-ts/components/EditModal.tsx
@@ -2,20 +2,31 @@ import React from 'react';
 import { Todo } from '../src/App';
 
 interface EditModalProps {
-    todo: Todo;
+    todo: {
+        todo_id: number;
+        name: string;
+        description?: string;
+        created_at: string;
+        due_date: string;
+        user_id: number;
+    };
 }
 
 export default function EditModal({ todo }: EditModalProps) {
     return (
         <div className="modal">
-            <form>
-                <input type="text" name="name" value={todo.name} />
-                <input
-                    type="text"
-                    name="description"
-                    value={todo.description}
-                />
-            </form>
+            <div className="modal-content">
+                <form>
+                    <input type="text" name="name" value={todo.name} />
+                    <input
+                        type="text"
+                        name="description"
+                        value={todo.description}
+                    />
+                </form>
+            </div>
+            <button className="edit-todo-btn">Edit Todo</button>
+            <button className="cancel-btn">Cancel</button>
         </div>
     );
 }
