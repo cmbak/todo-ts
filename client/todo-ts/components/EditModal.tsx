b2ca189b378792d450b58f72c784a6b6c7396b51
cmbak <chikmii@gmail.com>
2024-01-11 20:07:15 +0000

    Modal now opens and closes
    Moved EditTodo button to EditModal component so state change only happens there

--- client/todo-ts/components/EditModal.tsx
+++ client/todo-ts/components/EditModal.tsx
@@ -15,11 +15,12 @@ export default function EditModal({ todo }: EditModalProps) {
     const [name, setName] = useState(todo.name);
     const [description, setDescription] = useState(todo.description || ''); // Need ''?
     // todo date?
+    const [display, setDisplay] = useState(false);
 
     async function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
         event.preventDefault();
 
-        console.log(`Edit Todo ${name} ${description}`);
+        // console.log(`Edit Todo ${name} ${description}`);
         await fetch(`http://localhost:3000/todos/${todo.todo_id}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
@@ -29,44 +30,65 @@ export default function EditModal({ todo }: EditModalProps) {
             }),
         });
 
-        // closeModal()
+        closeModal();
     }
 
     function handleCancelClick() {
-        console.log('Close modal');
-        // closeModal()
+        closeModal();
+    }
+
+    function handleShowClick() {
+        setDisplay(true);
+    }
+
+    function closeModal() {
+        setDisplay(false);
     }
 
     return (
-        <div className="modal-container">
-            <div className="modal">
-                <div className="modal-content">
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            name="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            name="description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                        <button className="edit-todo-btn" type="submit">
-                            Edit Todo
-                        </button>
-                        <button
-                            className="cancel-btn"
-                            onClick={handleCancelClick}
-                        >
-                            Cancel
-                        </button>
-                    </form>
-                </div>
+        <>
+            <div className="">
+                <button
+                    className="todo-btn edit-btn"
+                    onClick={() => handleShowClick()}
+                >
+                    Edit Todo
+                </button>
             </div>
-            <div className="overlay active"></div>
-        </div>
+            <div
+                className={`modal-container ${
+                    display == false ? 'hidden' : ''
+                }`}
+            >
+                <div className="modal">
+                    <div className="modal-content">
+                        <form onSubmit={(e) => handleSubmit(e)}>
+                            <input
+                                type="text"
+                                name="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <input
+                                type="text"
+                                name="description"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                            <button className="edit-todo-btn" type="submit">
+                                Edit Todo
+                            </button>
+                            <button
+                                className="cancel-btn"
+                                onClick={handleCancelClick}
+                            >
+                                Cancel
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div className="overlay active"></div>
+            </div>
+        </>
     );
 }
--- client/todo-ts/components/EditTodoBtn.tsx
+++ client/todo-ts/components/EditTodoBtn.tsx
@@ -1,35 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TodoBtnProps } from './TodoItem';
 import EditModal from './EditModal';
 
 export default function EditTodoBtn({ todo }: TodoBtnProps) {
-    const [showModal, setShowModal] = useState(false);
-
-    // async function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
-    //     event.preventDefault();
-
-    //     await fetch(`http://localhost:3000/todos/${todo.todo_id}`, {
-    //         method: 'PUT',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify({
-    //             name: name,
-    //             description: description,
-    //         }),
-    //     });
-    // }
-
-    function handleClick() {
-        console.log(todo.todo_id);
-        setShowModal((prevShowModal) => !prevShowModal);
-    }
-
-    return (
-        <>
-            <button className="todo-btn edit-btn" onClick={handleClick}>
-                Edit Todo
-            </button>
-            {/* TODO make modal separate component? */}
-            {showModal && <EditModal key={todo.todo_id} todo={todo} />}
-        </>
-    );
+    return <EditModal key={todo.todo_id} todo={todo} />;
 }
