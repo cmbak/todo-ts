3bca62ca3793a4a3413a3f625d1d742512239f3a
cmbak <chikmii@gmail.com>
2024-01-14 21:17:29 +0000

    Edit form doesn't get submitted when cancel button pressed

--- client/todo-ts/components/EditModal.tsx
+++ client/todo-ts/components/EditModal.tsx
@@ -91,6 +91,7 @@ export default function EditModal({ todo, editTodo }: EditModalProps) {
                                 Edit Todo
                             </button>
                             <button
+                                type="button"
                                 className="cancel-btn"
                                 onClick={() => handleCancelClick()}
                             >
