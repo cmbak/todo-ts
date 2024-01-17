5a466e293ed6c3bb2b6125e5f632ebc41dba30a1
cmbak <chikmii@gmail.com>
2024-01-17 21:30:01 +0000

    Made todo-container responsive on mobile

--- client/todo-ts/components/Countdown.tsx
+++ client/todo-ts/components/Countdown.tsx
@@ -8,7 +8,7 @@ interface CountdownProps {
 export default function Countdown({ dueDate }: CountdownProps) {
     const daysLeft = getDaysLeft(dueDate);
     const isOverdue = getIsOverdue(daysLeft);
-    const dayText = daysLeft == 1 ? 'day' : 'days';
+    const dayText = Math.abs(daysLeft) == 1 ? 'day' : 'days';
 
     function getIsOverdue(daysLeft: number) {
         return daysLeft < 0 ? true : false;
